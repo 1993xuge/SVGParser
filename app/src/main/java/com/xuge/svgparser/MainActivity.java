64c551321768d515143a25ec48bd21dd8ca995b5
yixu.wang <yixu.wang@ihandysoft.com>
2019-04-12 18:32:29 +0800

    add lib svg

--- app/src/main/java/com/xuge/svgparser/MainActivity.java
+++ app/src/main/java/com/xuge/svgparser/MainActivity.java
@@ -3,11 +3,16 @@ package com.xuge.svgparser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.xuge.libsvg.SVGImageView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SVGImageView imageView = findViewById(R.id.svg);
+        imageView.setImageResource(R.raw.tarot_13);
     }
 }
